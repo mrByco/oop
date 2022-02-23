9c8301eff5c83d9ad9517b8ad47ec1bc8fde8901
Mátyás <matyi0918@gmail.com>
2022-02-23 20:35:42 +0100

    Calculator type 1

---
+++ gyak03/src/me/matyi/EOperand.java
@@ -0,0 +1,9 @@
+package me.matyi;
+
+public enum EOperand {
+    multiply,
+    add,
+    subtract,
+    divide,
+    mod
+}
--- gyak03/src/me/matyi/Main.java
+++ gyak03/src/me/matyi/Main.java
@@ -1,51 +1,92 @@
 package me.matyi;
 
-import jdk.jshell.spi.ExecutionControl;
-
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        IContact contact = new Contact();
-
-        IContact2 contact2 = (IContact2) contact;
-    }
-}
-
-interface IContact {
-    void printMails();
-}
-interface IContact2 {
-    void printAllMails();
-}
-
-class Contact implements IContact, IContact2 {
-    String firstName;
-    List<String> mails;
-
-    public void printMails(){
-
-    }
-    public void printAllMails(){
-
-    }
-}
-class OtherContact implements IContact {
-    String displayName;
-    OtherContact(String displayName){
-        this.displayName = displayName;
+        startCalculationLoop();
     }
 
-    public void printMails(){
-        System.out.println("Animal type changed to: ");
+    public static void startCalculationLoop(){
+        boolean doNextCalculation = true;
+        while (doNextCalculation){
+            executeCalculationLoop();
+            doNextCalculation = askForContinue();
+        }
     }
-    public void printMails5(){
-        System.out.println("Animal type changed to: ");
+
+    public static void executeCalculationLoop(){
+        System.out.println("Give me number A");
+        int a = readNumber();
+
+        System.out.println("Give me the operand");
+        EOperand operand = readOperand();
+
+        System.out.println("Give me number B");
+        int b = readNumber();
+
+        System.out.println(calculate(a, b, operand));
     }
-    public void printMails7(){
-        System.out.println("Animal type changed to: ");
+
+    public static int readNumber(){
+        int number = 0;
+        while (!scanner.hasNextInt()){
+            System.out.println("Give me a number");
+            scanner.nextLine();
+        }
+        number = scanner.nextInt();
+        scanner.nextLine();
+        return number;
+    }
+
+    public static EOperand readOperand() {
+        EOperand operand = null;
+        while (operand == null){
+            String input = scanner.nextLine();
+            switch (input) {
+                case "+" -> operand = EOperand.add;
+                case "-" -> operand = EOperand.subtract;
+                case "*" -> operand = EOperand.multiply;
+                case "/" -> operand = EOperand.divide;
+                case "%" -> operand = EOperand.mod;
+                default -> System.out.println("Please enter a valid operand ('+', '-', '*', '/', '%')");
+            }
+        }
+        return operand;
+    }
+
+    public static double calculate(int a, int b, EOperand operand) throws IllegalStateException {
+        switch (operand){
+            case multiply -> {
+                return a * b;
+            }
+            case add -> {
+                return a + b;
+            }
+            case subtract -> {
+                return a - b;
+            }
+            case divide -> {
+                return (double)a / b;
+            }
+            case mod -> {
+                return a % b;
+            }
+        }
+        throw new IllegalStateException("Unknown operand: " + operand);
+    }
+
+    public static boolean askForContinue(){
+        while (true){
+            System.out.println("Next calculation? (Y/n)");
+            String answer = scanner.nextLine().toLowerCase();
+            if (answer.equals("y") || answer.equals("n") || answer.equals("")){
+                return !answer.equals("n");
+            }
+            System.out.println("Invalid answer");
+        }
     }
 }
